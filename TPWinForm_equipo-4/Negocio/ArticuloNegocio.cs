576f011004e4aaa09b4d26d01f22a17241ad07ff
Santiago Lapeña <slapena88@gmail.com>
2023-09-12 11:37:25 +0200

    Coneccion a BD

---
+++ TPWinForm_equipo-4/Negocio/ArticuloNegocio.cs
@@ -0,0 +1,56 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar ()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = "SELECT Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio FROM ARTICULOS";
+
+                datos.setConsulta(consulta);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.ID = (int)datos.Lector["Id"];
+                    aux.codigo = (string)datos.Lector["Codigo"];
+                    aux.nombre = (string)datos.Lector["Nombre"];
+                    aux.descripcion = (string)datos.Lector["Descripcion"];
+                    //aux.marca = 
+                    //aux.categoria =
+                    aux.precio = (decimal)datos.Lector["Precio"];
+                    //aux.imagenURL = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+    }
+}
--- TPWinForm_equipo-4/Vista/Form1.cs
+++ TPWinForm_equipo-4/Vista/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dominio;
+using Negocio;
 
 namespace Vista
 {
     public partial class Form1 : Form
     {
+        private List<Articulo> listArticulo;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio art = new ArticuloNegocio();
+            listArticulo = art.listar();
+            dgvArticulo.DataSource = listArticulo;
         }
     }
 }
